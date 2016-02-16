7b6c2948cb414a04907f04df5a4ca519b730d02e
Tom Maxwell <tom.maxwell83@gmail.com>
2016-02-16 21:25:59 +0000

    show some floor fill
    mend

--- Tourqbot/Assets/Tourqbot Prefabs/Infinite Floor/InfiniteFloorBehaviour.cs
+++ Tourqbot/Assets/Tourqbot Prefabs/Infinite Floor/InfiniteFloorBehaviour.cs
@@ -5,68 +5,77 @@ using System.Linq;
 public class InfiniteFloorBehaviour : MonoBehaviour
 {
     private GameObject theBot;
-    private Transform botTrans;
-    private Rigidbody2D botBody;
+    //private Transform botTrans;
+    //private Rigidbody2D botBody;
     private LineRenderer lineRenderer;
+    private MeshRenderer meshRenderer;
+    private EdgeCollider2D edge;
 
     // Use this for initialization
     void Start()
     {
-        theBot = GameObject.Find("TheBot");
-        botTrans = theBot.GetComponent<Transform>();
+        //theBot = GameObject.Find("TheBot");
+        //botTrans = theBot.GetComponent<Transform>();
 
-        botBody = theBot.GetComponent<Rigidbody2D>();
+        //botBody = theBot.GetComponent<Rigidbody2D>();
         lineRenderer = GetComponent<LineRenderer>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        edge = GetComponent<EdgeCollider2D>();
 
+
+        setUpFill();
         setUpLines();
-        //var coliderPoints = GetComponent<EdgeCollider2D>().points.Cast<Vector3>();
-
-        //Debug.Log("in render");
-
-        //lineRenderer.useWorldSpace = false;
-        //lineRenderer.SetPositions(coliderPoints.ToArray());
-
     }
 
     private void setUpLines()
     {
         var coliderPoints = GetComponent<EdgeCollider2D>().points.Select(p => new Vector3(p.x, p.y, 0)).ToArray();
 
-        //if (coliderPoints.Length < 2)
-        //    return;
-
-        Debug.Log("in render");
-
         lineRenderer.useWorldSpace = false;
         lineRenderer.SetVertexCount(coliderPoints.Length);
         lineRenderer.SetPositions(coliderPoints.ToArray());
 
-        lineRenderer.SetColors(Color.cyan, Color.green);
+        lineRenderer.SetColors(Color.green, Color.green);
         lineRenderer.SetWidth(0.2F, 0.2F);
 
+        //Material mat = new Material(Shader.Find("Particles/Additive"));
+        //mat.color = Color.green;
+        //lineRenderer.material = mat;
+    }
 
-        //for (var n = 0; n < coliderPoints.Length - 1; n++)
-        //{
-        //    var firstPoint = coliderPoints[n];
-        //    var secondPoint = coliderPoints[n + 1];
+    private void OnDestroy()
+    {
+        Destroy(GetComponent<Renderer>().material);
+    }
 
-        //    var drawFrom = transform.position + new Vector3(firstPoint.x, firstPoint.y, 0);
-        //    var drawTo = transform.position + new Vector3(secondPoint.x, secondPoint.y, 0);
+    private void setUpFill()
+    {
+        var point = GetComponent<EdgeCollider2D>().points.Select(p => new Vector3(p.x, p.y, 0)).ToArray();
 
-        //    Graphics.dr.DrawLine(drawFrom, drawTo);
-        //}
+        const int ySize = 5;
+        var xSize = point.Length - 1;
+
+        var mesh = GetComponent<MeshFilter>().mesh = new Mesh();
+        mesh.name = "Procedural Grid";
+
+		var vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+		for (int i = 0, y = 0; y <= ySize; y++) {
+			for (int x = 0; x <= xSize; x++, i++) {
+				vertices[i] = point[x] + new Vector3(0, (y-ySize)*3); 
+			}
+		}
+		mesh.vertices = vertices;
+
+		int[] triangles = new int[xSize * ySize * 6];
+		for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++) {
+			for (int x = 0; x < xSize; x++, ti += 6, vi++) {
+				triangles[ti] = vi;
+				triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+				triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+				triangles[ti + 5] = vi + xSize + 2;
+			}
+		}
+		mesh.triangles = triangles;
+        
     }
 }
-
-    //void OnTriggerEnter2D(Collider2D other)
-    //{
-    //    botBody.freezeRotation = true;
-    //    botBody.angularVelocity = 0;
-    //    botBody.velocity = new Vector2(0, 0);
-
-    //    botTrans.position = Restart;
-    //    botBody.velocity = new Vector2(0, 4);
-    //    botBody.freezeRotation = false;
-
-    //}
-
